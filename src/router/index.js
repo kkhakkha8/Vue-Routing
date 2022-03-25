7ca7f52e19f00faff649cd32e64eafb310631a32
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-03-25 18:15:39 +0600

    routing intro

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import About from "../views/AboutView.vue"
+import AboutView from "../views/AboutView.vue"
 
 const routes = [
   {
@@ -11,7 +11,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    component: About
+    component: AboutView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
